d063c14de134bd86f21b12b4f2a50d7151de3b84
FlowerOnStone <thuanbn03@gmail.com>
2022-10-13 00:04:09 +0700

    add BFSTrace

--- src/main/java/Trace/BfsTrace.java
+++ src/main/java/Trace/BfsTrace.java
@@ -3,9 +3,13 @@ package Trace;
 import Constants.Constants;
 import Entities.Animate.Character.Bomber;
 import Entities.Animate.Character.Enemy.Enemy;
+import Entities.Still.Grass;
+import Graphics.Sprite;
 import Map.Map;
 import javafx.util.Pair;
+import jdk.jfr.consumer.RecordedThreadGroup;
 
+import javax.management.QueryEval;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,58 +17,80 @@ import static Constants.Constants.*;
 import static Constants.Constants.DIRECTION.*;
 
 public class BfsTrace extends Trace {
-    private static final boolean[][] distance = new boolean[HEIGHT][WIDTH];
 
     public BfsTrace(Bomber player, Enemy enemy, Map gameMap) {
         super(player, enemy, gameMap);
-        for (int i = 0; i < HEIGHT; ++i) {
-            for (int j = 0; j < WIDTH; ++j) {
-                distance[i][j] = false;
-            }
-        }
-    }
-
-    private Pair<Integer, Integer> Bfs(Bomber bomber, Enemy enemy) {
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
-        Pair<Integer, Integer> start = bomber.getTile();
-        Pair<Integer, Integer> end = enemy.getTile();
-        Pair<Integer, Integer> trace = null;
-        queue.add(start);
-        distance[start.getKey()][start.getValue()] = true;
-        boolean stop = false;
-        while (!queue.isEmpty() && !stop) {
-            Pair<Integer, Integer> tile = queue.remove();
-            int x = tile.getKey();
-            int y = tile.getValue();
-            for (int i = 0; i < 4; ++i) {
-                int u = x + dx[i];
-                int v = y + dy[i];
-                if (isMovable(u, v) && !distance[u][v]) {
-                    if (u == end.getKey() && v == end.getValue()) {
-                        trace = new Pair<>(x, y);
-                        stop = true;
-                        break;
-                    }
-                    queue.add(new Pair<>(u, v));
-                    distance[u][v] = true;
-                }
-            }
-        }
-        return trace;
     }
 
     @Override
-    public DIRECTION getDirection() {
-        Pair<Integer, Integer> trace = Bfs(player, enemy);
-        if (trace == null) {
-            return new RandomTrace(player, enemy, gameMap).getDirection();
+    public DIRECTION trace() {
+        if (enemy.getPixelX() % Sprite.SCALED_SIZE == 0 || (enemy.getPixelY() & Sprite.SCALED_SIZE) == 0) {
+            return enemy.getDirection();
         }
-        int _x = trace.getKey() - enemy.getTile().getKey();
-        int _y = trace.getValue() - enemy.getTile().getValue();
-        if (_x == -1 && _y == 0) return UP;
-        if (_x == 1 && _y == 0) return DOWN;
-        if (_x == 0 && _y == -1) return LEFT;
-        if (_x == 0 && _y == 1) return RIGHT;
-        return DOWN;
+        if (playerDistance() < 2 * sqr(Sprite.SCALED_SIZE)) {
+            return new DistanceTrace(player, enemy, gameMap).trace();
+        }
+
+        int lengthBomb = player.getLengthFlame() + 1;
+        Queue<Integer> direc = new LinkedList<>();
+        Queue<Integer> tileX = new LinkedList<>();
+        Queue<Integer> tileY = new LinkedList<>();
+        boolean[][] checkPass = new boolean[WIDTH][HEIGHT];
+        boolean checkFaceBooms = checkFaceBoom(enemy.getTileX(), enemy.getTileY(), lengthBomb);
+        int directionDodge = -1;
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                checkPass[i][j] = false;
+            }
+        }
+        checkPass[enemy.getTileX()][enemy.getTileY()] = true;
+        for (int direction = 0; direction < DIRECTION_LABEL.length; direction++) {
+            int x = enemy.getTileX() + DIRECTION_X[direction];
+            int y = enemy.getTileY() + DIRECTION_Y[direction];
+            if (gameMap.getTiles(x, y) instanceof Grass) {
+                direc.add(direction);
+                tileX.add(x);
+                tileY.add(y);
+                checkPass[x][y] = true;
+            }
+        }
+        while (!direc.isEmpty()) {
+            int x = tileX.poll(), y = tileY.poll(), directFirst = direc.poll();
+            if (directionDodge == -1 && !checkFaceBoom(enemy.getTileX() + DIRECTION_X[directFirst],
+                    enemy.getTileY() + DIRECTION_Y[directFirst], lengthBomb)) {
+                directionDodge = directFirst;
+            }
+            if (checkFaceBooms && !checkFaceBoom(x, y, lengthBomb)) {
+               return DIRECTION_LABEL[directFirst];
+            }
+            if (x == player.getTileX() && y == player.getTileY()
+                    && !checkFaceBoom(enemy.getTileX() + DIRECTION_X[directFirst],
+                    enemy.getTileY() + DIRECTION_Y[directFirst], lengthBomb)) {
+                return  DIRECTION_LABEL[directFirst];
+            }
+            for (int direction = 0; direction < DIRECTION_LABEL.length; direction++) {
+                int u = x + DIRECTION_X[direction];
+                int v = y + DIRECTION_Y[direction];
+                if (!checkPass[u][v] && gameMap.getTiles(u, v) instanceof Grass) {
+                    direc.add(directFirst);
+                    tileX.add(u);
+                    tileY.add(v);
+                    checkPass[u][v] = true;
+                }
+            }
+        }
+        DIRECTION directionRandom = new RandomTrace(player, enemy, gameMap).trace();
+        int directionRandomId = 0;
+        for (int id = 0; id < DIRECTION_LABEL.length; id++){
+            if (directionRandom == DIRECTION_LABEL[id]){
+                directionRandomId = id;
+                break;
+            }
+        }
+        if (directionDodge == -1 && !checkFaceBoom(enemy.getTileX() + DIRECTION_X[directionRandomId],
+                enemy.getTileY() + DIRECTION_Y[directionRandomId], lengthBomb)) {
+            return directionRandom;
+        }
+        return DIRECTION_LABEL[directionDodge];
     }
 }
